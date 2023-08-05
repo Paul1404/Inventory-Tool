78fa5e1e3dfdf8d21202c7cc56319236433261b9
Paul <97813342+Paul1404@users.noreply.github.com>
2023-08-06 00:45:53 +0200

    Update .gitignore + Parse-Inventory

--- Parse-Inventory.ps1
+++ Parse-Inventory.ps1
@@ -4,6 +4,22 @@ Add-Type -AssemblyName System.Windows.Forms
 # Define the file path
 $filePath = ".\inventory.json"
 
+function ConvertTo-Hashtable {
+    param($InputObject)
+
+    $hashTable = @{}
+
+    $InputObject.PSObject.Properties | ForEach-Object {
+        $hashTable[$_.Name] = $_.Value
+    }
+
+    return $hashTable
+}
+
+
+# Load settings from JSON file
+$settings = Get-Content -Path '.\settings.json' | ConvertFrom-Json
+
 # Create a DataTable
 $dataTable = New-Object System.Data.DataTable
 
@@ -41,53 +57,42 @@ function Get-Data {
 }
 
 # Create a DataGridView and add data
-$dataGridView = New-Object System.Windows.Forms.DataGridView -Property @{
-    AutoSizeColumnsMode = 'AllCells'
-    Dock = 'Fill'
-    BackgroundColor = 'White'
-    GridColor = 'LightGray'
-    ColumnHeadersDefaultCellStyle = New-Object System.Windows.Forms.DataGridViewCellStyle -Property @{
-        BackColor = 'Navy'
-        ForeColor = 'White'
+$dataGridView = New-Object System.Windows.Forms.DataGridView -Property (ConvertTo-Hashtable $settings.DataGridView)
+
+# Function to create a button
+function New-Button {
+    param([string]$text, [scriptblock]$onClick)
+
+    return New-Object System.Windows.Forms.Button -Property @{
+        Text = $text
+        Dock = $settings.Button.Dock
+        BackColor = $settings.Button.BackColor
+        ForeColor = $settings.Button.ForeColor
+        Font = New-Object System.Drawing.Font($settings.Button.Font, $settings.Button.FontSize)  # Set the font and size
+        Add_Click = $onClick
     }
-    DefaultCellStyle = New-Object System.Windows.Forms.DataGridViewCellStyle -Property @{
-        BackColor = 'WhiteSmoke'
-        ForeColor = 'Black'
+}
+
+# Create buttons
+$clearButton = New-Button 'Clear Inventory' {
+    $result = [System.Windows.Forms.MessageBox]::Show("Are you sure you want to clear the inventory?", "Confirm", [System.Windows.Forms.MessageBoxButtons]::YesNo, [System.Windows.Forms.MessageBoxIcon]::Warning)
+
+    if ($result -eq 'Yes') {
+        if (Test-Path $filePath) {
+            Remove-Item -Path $filePath
+        }
+
+        Get-Data
     }
-    AlternatingRowsDefaultCellStyle = New-Object System.Windows.Forms.DataGridViewCellStyle -Property @{
-        BackColor = 'Lavender'
-        ForeColor = 'Black'
-    }
-    RowHeadersVisible = $false
-}
+} (ConvertTo-Hashtable $settings.Button)
 
-# Create a button to clear inventory
-$clearButton = New-Object System.Windows.Forms.Button -Property @{
-    Text = 'Clear Inventory'
-    Dock = 'Fill'
-    BackColor = 'Navy'
-    ForeColor = 'White'
-    Font = New-Object System.Drawing.Font("Arial", 15)  # Set the font and size
-}
+$refreshButton = New-Button 'Refresh Data' {
+    Get-Data
+} (ConvertTo-Hashtable $settings.Button)
 
-# Create a button to refresh the data
-$refreshButton = New-Object System.Windows.Forms.Button -Property @{
-    Text = 'Refresh Data'
-    Dock = 'Fill'
-    BackColor = 'Navy'
-    ForeColor = 'White'
-    Font = New-Object System.Drawing.Font("Arial", 15)  # Set the font and size
-}
-
-# Create a button to close the application
-$closeButton = New-Object System.Windows.Forms.Button -Property @{
-    Text = 'Close Application'
-    Dock = 'Fill'
-    BackColor = 'Navy'
-    ForeColor = 'White'
-    Font = New-Object System.Drawing.Font("Arial", 15)  # Set the font and size
-}
-
+$closeButton = New-Button 'Close Application' {
+    $form.Close()
+} (ConvertTo-Hashtable $settings.Button)
 
 # Create a panel and add buttons
 $buttonPanel = New-Object System.Windows.Forms.TableLayoutPanel
@@ -100,33 +105,6 @@ $buttonPanel.Controls.Add($closeButton, 2, 0)  # Add close button in the third c
 $buttonPanel.ColumnStyles.Add((New-Object System.Windows.Forms.ColumnStyle([System.Windows.Forms.SizeType]::Percent, 33.33)))  # Set width to 33.33%
 $buttonPanel.ColumnStyles.Add((New-Object System.Windows.Forms.ColumnStyle([System.Windows.Forms.SizeType]::Percent, 33.33)))  # Set width to 33.33%
 $buttonPanel.ColumnStyles.Add((New-Object System.Windows.Forms.ColumnStyle([System.Windows.Forms.SizeType]::Percent, 33.33)))  # Add a new ColumnStyle for the third column
-
-
-# Define what happens when the clear button is clicked
-$clearButton.Add_Click({
-    # Confirm with the user
-    $result = [System.Windows.Forms.MessageBox]::Show("Are you sure you want to clear the inventory?", "Confirm", [System.Windows.Forms.MessageBoxButtons]::YesNo, [System.Windows.Forms.MessageBoxIcon]::Warning)
-
-    if ($result -eq 'Yes') {
-        # Clear the file
-        if (Test-Path $filePath) {
-            Remove-Item -Path $filePath
-        }
-
-        # Refresh the data
-        Get-Data
-    }
-})
-
-# Define what happens when the refresh button is clicked
-$refreshButton.Add_Click({
-    Get-Data
-})
-
-# Define what happens when the close button is clicked
-$closeButton.Add_Click({
-    $form.Close()
-})
 
 # Create a form and add DataGridView and button panel
 $form = New-Object System.Windows.Forms.Form -Property @{
